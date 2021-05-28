c8fbe176e1ab378b7ef61ad04d2f1ef04d2e4c1c
Rory Jarrard <rjarrard@frontsteps.com>
2021-05-28 10:58:35 -0600

    add page structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react'
-import { FormControl, Select, MenuItem } from '@material-ui/core'
+import {
+  Card,
+  CardContent,
+  FormControl,
+  Select,
+  MenuItem,
+} from '@material-ui/core'
 import './App.css'
 
 import InfoBox from './components/InfoBox'
@@ -33,29 +39,40 @@ function App() {
 
   return (
     <div className='app'>
-      <div className='app__header'>
-        <h1>COVID-19 TRACKER</h1>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19 TRACKER</h1>
 
-        <FormControl className='app__dropdown'>
-          <Select variant='outlined' value={country} onChange={onCountryChange}>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+          <FormControl className='app__dropdown'>
+            <Select
+              variant='outlined'
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value='worldwide'>Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='app__stats'>
+          <InfoBox title='Coronavirus Cases' cases={123} total={2000} />
+          <InfoBox title='Recovered' cases={1234} total={3000} />
+          <InfoBox title='Deaths' cases={12345} total={4000} />
+        </div>
+        <Map />
       </div>
 
-      <div className='app__stats'>
-        <InfoBox title='Coronavirus Cases' cases={123} total={2000} />
-        <InfoBox title='Recovered' cases={1234} total={3000} />
-        <InfoBox title='Deaths' cases={12345} total={4000} />
-      </div>
-
-      {/* Table */}
-      {/* Graph */}
-
-      <Map />
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <h3>Worldwide New Cases</h3>
+        </CardContent>
+        {/* Table */}
+        {/* Graph */}
+      </Card>
     </div>
   )
 }
