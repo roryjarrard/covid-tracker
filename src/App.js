12326c457a02ae120c0550254a74e29dcf1f0434
Rory Jarrard <rjarrard@frontsteps.com>
2021-05-28 08:40:26 -0600

    select wireframe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
+import { FormControl, Select, MenuItem } from '@material-ui/core'
 import './App.css'
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'India'])
+
   return (
     <div className='app'>
-      <h1>Let's build a Covid 19 tracker</h1>
+      <div className='app__header'>
+        <h1>COVID-19 TRACKER</h1>
+
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' value='abc' onChange={() => {}}>
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field  */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   )
 }
